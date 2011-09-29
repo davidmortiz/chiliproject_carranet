6bc8882c7fa5ca8d648ea6b03516e654f7197b9a
Dave Ortiz <daveortiz@gmail.com>
2011-09-29 12:55:26 -0400

    changes

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'redmine'
 
 Redmine::Plugin.register :chiliproject_carranet do
   name 'Chiliproject Carranet plugin'
-  author 'Author name'
-  description 'This is a plugin for ChiliProject'
+  author 'David Ortiz'
+  description 'Chiliproject Plgin'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'http://carranetwork.org'
+  author_url 'http://github.org/davidmortiz'
 end
